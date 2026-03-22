28e6b40c1c08bad392208b81f455ceb08f484235
urbanopublicidadego-debug <urbanopublicidadego@gmail.com>
2026-03-22 17:01:59 -0300

    Add caching functionality to service worker
    Implement basic caching for assets in service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
-self.addEventListener('fetch', function(event) {
-  // O Service Worker básico apenas para permitir a instalação
+const cacheName = 'cv-v1';
+const assets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
