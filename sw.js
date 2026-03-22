64886ee17130d02fd6665ad16b153b8d34a0a3a7
urbanopublicidadego-debug <urbanopublicidadego@gmail.com>
2026-03-22 17:09:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,36 @@
-const cacheName = 'cv-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'cv-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+// Instalação: Salva os arquivos essenciais
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting(); // Força a atualização imediata
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Ativação: Limpa caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: Responde mesmo se estiver offline (O que o Chrome exige)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
